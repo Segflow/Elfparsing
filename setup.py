26b1a4cd0275da6a5bacb07bd533d1ea5bde805d
xGeek <assel.meher@gmail.com>
2012-07-30 22:54:51 +0000

    Install script added

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup (
+	name='Elfparsing',
+	version='1.0',
+	description='Python module for parsing Elf format 32 and 64 bits',
+	url='https://github.com/JonathanSalwan/Elfparsing',
+	packages=['Elfparsing']
+	)
